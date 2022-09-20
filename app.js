6fbe81d0b5d6c6fb4dd400f450153938ce0825ff
sejung1997 <ksgve1997814@naver.com>
2022-09-20 23:32:19 +0900

    get/post

--- app.js
+++ app.js
@@ -1,12 +1,51 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "jade"); // express jade 연결
 app.set("views", "./views");
 app.locals.pretty = true;
 app.use(express.static("public"));
 app.get("/template", (req, res) => {
-  res.render("temp", { time: "hello" }); // views 폴더 안에 temp라는 파일 찾음
+  res.render("temp", { time: Date(), title: "jade" }); // views 폴더 안에 temp라는 파일 찾음
 });
+app.get("/form", (req, res) => {
+  res.render("form");
+});
+app.get("/form_receiver", (req, res) => {
+  const title = req.query.title;
+  const description = req.query.description;
+  res.send(title + "," + description);
+});
+app.post("/form_receiver", (req, res) => {
+  const title = req.body.title;
+  const description = req.body.description;
+  res.send(title + "," + description);
+});
+app.get("/topic/:id", (req, res) => {
+  const as = `
+    <a href="/topic?id=0">javaScript</a><br/>
+    <a href="/topic?id=1">node</a><br/>
+    <a href="/topic?id=2">express</a><br/>
+    // ${req.query.id}
+    ${req.params.id}
+  `;
+  res.send(as);
+});
+// app.get("/topic", (req, res) => {
+//   const as = `
+//     <a href="/topic?id=0">javaScript</a><br/>
+//     <a href="/topic?id=1">node</a><br/>
+//     <a href="/topic?id=2">express</a><br/>
+//     // ${req.query.id}
+//     ${req.query.id}
+//   `;
+//   res.send(as);
+// });
+app.get("/topic/:id/:mode", (req, res) => {
+  res.send(req.params.id + "," + req.params.mode);
+});
+
 app.get("/", (req, res) => {
   console.log(req);
   console.log(res);
